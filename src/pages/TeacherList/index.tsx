2a1995035f703ceaa821c3563e897e9c4b6eb154
Ivan Seibel <ivanseibel@gmail.com>
2020-08-03 15:22:56 -0300

    feature: add teacher description.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -4,6 +4,8 @@ import PageHeader from '../../components/PageHeader'
 
 import './styles.css'
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
@@ -23,6 +25,35 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+      
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars0.githubusercontent.com/u/42596775?s=460&u=8ddc06cf5793a75d7e7ad462ddfed52b8ef4d503&v=4" alt="Ivan Seibel"/>
+            <div>
+              <strong>Ivan Seibel</strong>
+              <span>Chemistry</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br/><br/>
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200 mil pessoas já passaram por uma das minhas explosões.
+          </p>
+
+          <footer>
+            <p>
+              Price/hour
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Get in touch
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
