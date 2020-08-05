63ec98e3a2c66c3eb04dfa1d4a765561c33ec559
Ivan L. Seibel <ivan@pop-os.localdomain>
2020-08-05 10:45:54 -0300

    Refactor with new Input component.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -5,24 +5,18 @@ import PageHeader from '../../components/PageHeader';
 import './styles.css';
 
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="These are the available teachers.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Subject</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Week day</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Time</label>
-            <input type="text" id="time" />
-          </div>
+          <Input label="Subject" name="subject" />
+
+          <Input label="Week day" name="week_day" />
+
+          <Input label="Time" type="time" name="time" />
         </form>
       </PageHeader>
 
