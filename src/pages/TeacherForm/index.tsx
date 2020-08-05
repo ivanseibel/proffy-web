0fe45ebb83b0f9b92c98e9469a451c7d573c5900
Ivan L. Seibel <ivan@pop-os.localdomain>
2020-08-05 15:44:05 -0300

    Implement add new available times.

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
@@ -7,7 +7,24 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import TextArea from '../../components/TextArea';
 import Select from '../../components/Select';
 
+interface IScheduleItem {
+  week_day: number | null;
+  from: string;
+  to: string;
+}
+
 const TeacherForm: React.FC = () => {
+  const [scheduleItems, setScheduleItems] = useState<IScheduleItem[]>([
+    { week_day: null, from: '', to: '' },
+  ]);
+
+  const addNewScheduleItem = useCallback(
+    (scheduleItem: IScheduleItem) => {
+      setScheduleItems([...scheduleItems, scheduleItem]);
+    },
+    [scheduleItems],
+  );
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
@@ -45,33 +62,51 @@ const TeacherForm: React.FC = () => {
             ]}
           />
 
-          <Input label="Hour/class cost" name="cost" marginTop="1.4rem" />
+          <Input
+            label="Cost (US$/hour)"
+            type="number"
+            name="cost"
+            marginTop="1.4rem"
+          />
         </fieldset>
 
         <fieldset>
           <legend>
             Available times
-            <button type="button">+ New time</button>
+            <button
+              type="button"
+              onClick={() => {
+                addNewScheduleItem({
+                  week_day: null,
+                  from: '',
+                  to: '',
+                });
+              }}
+            >
+              + New time
+            </button>
           </legend>
 
-          <div className="schedule-item">
-            <Select
-              label="Week day"
-              name="week_day"
-              options={[
-                { value: '0', label: 'Sunday' },
-                { value: '1', label: 'Monday' },
-                { value: '2', label: 'Tuesday' },
-                { value: '3', label: 'Wednesday' },
-                { value: '4', label: 'Thursday' },
-                { value: '5', label: 'Friday' },
-                { value: '6', label: 'Saturday' },
-              ]}
-            />
+          {scheduleItems.map((scheduleItem, index) => (
+            <div className="schedule-item" key={String(index)}>
+              <Select
+                label="Week day"
+                name="week_day"
+                options={[
+                  { value: '0', label: 'Sunday' },
+                  { value: '1', label: 'Monday' },
+                  { value: '2', label: 'Tuesday' },
+                  { value: '3', label: 'Wednesday' },
+                  { value: '4', label: 'Thursday' },
+                  { value: '5', label: 'Friday' },
+                  { value: '6', label: 'Saturday' },
+                ]}
+              />
 
-            <Input name="from" label="From" type="time" marginTop="1.4rem" />
-            <Input name="to" label="To" type="time" marginTop="1.4rem" />
-          </div>
+              <Input name="from" label="From" type="time" marginTop="1.4rem" />
+              <Input name="to" label="To" type="time" marginTop="1.4rem" />
+            </div>
+          ))}
         </fieldset>
 
         <footer>
