0e6529e7219237a299a155e7bba82a1186eb31a5
Ivan L. Seibel <ivan@pop-os.localdomain>
2020-08-05 16:03:37 -0300

    Configure toastify.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Routes: React.FC = () => {
   return (
@@ -11,6 +13,7 @@ const Routes: React.FC = () => {
 
       <Route path="/study" component={TeacherList} />
       <Route path="/teach" component={TeacherForm} />
+      <ToastContainer />
     </BrowserRouter>
   );
 };
